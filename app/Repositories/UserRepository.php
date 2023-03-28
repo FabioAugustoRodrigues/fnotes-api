b15ed4d2d68db55306676175cc6e63081056520b
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2023-03-28 14:57:14 -0300

    feat: created User repository

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository
+{
+    protected $model;
+
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+
+    public function getAll()
+    {
+        return $this->model->all();
+    }
+
+    public function getById($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $record = $this->getById($id);
+
+        return $record->update($data);
+    }
+
+    public function delete($id)
+    {
+        $record = $this->getById($id);
+
+        return $record->delete();
+    }
+}
